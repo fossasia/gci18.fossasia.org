7ddef579325051fcb26e0ca59ad917732fe77c62
Saarthak Chaturvedi <saarthakchats05@gmail.com>
2018-10-29 10:58:34 +0530

    Added Thank you section (#107)

---
+++ js/thankyou.js
@@ -0,0 +1,29 @@
+var items = [
+'Thank you for your contributions!',
+'Merci pour vos contributions!',
+'आपके योगदानों के लिए धन्यवाद',
+'Danke für Ihre Beiträge!'
+];
+
+var app = document.getElementById('app');
+var count = 0;
+var index = 0;
+var typingEffect = function typingEffect() {
+ var text = items[index];
+ if (count < text.length) {
+   setTimeout(function () {
+     app.innerHTML += text[count];
+     count++;
+     typingEffect();
+   }, Math.floor(Math.random(10) * 100));
+ } else {
+   count = 0;
+   index = index + 1 < items.length ? index + 1 : 0;
+   setTimeout(function () {
+     app.innerHTML = '';
+     typingEffect();
+   }, 1500);
+ }
+};
+
+typingEffect();
