22d6bf1fad5e918a8fd9447c38189551f70b1496
STEdirisuriya <supunedirisuriya2002@gmail.com>
2018-11-03 22:36:30 +0530

    improved and added sinhala for thank you..

--- js/thankyou.js
+++ js/thankyou.js
@@ -2,11 +2,13 @@
 'Thank you for your contributions!',
 'Merci pour vos contributions!',
 'आपके योगदानों के लिए धन्यवाद',
- 'Dziękujemy za Twój wkład!',
+'Dziękujemy za Twój wkład!',
 'Danke für Ihre Beiträge!',
 'Gracias por sus aportaciones!', 
 'तपाइको योगदानको लागि धन्यवाद',
-'شكرا للمساهمة','공헌 해 주셔서 감사합니다'
+'شكرا للمساهمة',
+'공헌 해 주셔서 감사합니다',
+'ඔබගේ දායකත්වයට ස්තූතියි!'
 ];
 
 var app = document.getElementById('app');
@@ -26,7 +28,7 @@ var typingEffect = function typingEffect() {
    setTimeout(function () {
      app.innerHTML = '';
      typingEffect();
-   }, 1500);
+   }, 1000);
  }
 };
 
