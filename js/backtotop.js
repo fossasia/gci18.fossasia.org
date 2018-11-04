353d38fd4ed2aadfa2ed16038daaa1a73f38cbbd
subhahu <34541684+subhahu123@users.noreply.github.com>
2018-11-04 17:50:09 +0530

    Update backtotop.js

--- js/backtotop.js
+++ js/backtotop.js
@@ -1,5 +1,9 @@
 var btn = $('#backtotop_button');
-
+<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 $(window).scroll(function() {
   if ($(window).scrollTop() > 300) {
     btn.addClass('show');
