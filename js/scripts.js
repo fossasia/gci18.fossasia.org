60a9f6a698f244682d98f905a0f0facdcc06618b
Saarthak Chaturvedi <saarthakchats05@gmail.com>
2018-11-05 23:39:47 +0530

    Fix Contributors' Section (#166)

--- js/scripts.js
+++ js/scripts.js
@@ -1404,7 +1404,7 @@ $(document).ready(function(){
       }).done(function(data){
         data.forEach(function(contributors){
           var html = '<div class="col-xs-6 col-sm-3 col-md-2 col-lg-2 text-center contributor"> <div class="card hvr-hang single-mentor">';
-          html += `<img src="https://github.com/${contributors.login}.png?size=120x120" height="120" width="120" style="width: auto">`;
+          html += '<img src="https://github.com/'+contributors.login+'.png?size=120x120" height="120" width="120" style="width: auto">';
           html += '<br>';
           $.ajax({
               url: "https://api.github.com/users/"+contributors.login
