7277bc9068d8ef2d980d0c5d6ca3fcf5e0f9ce60
Ammar <30975614+ammarbinfaisal@users.noreply.github.com>
2018-11-16 16:16:34 +0530

    fixes #297

--- js/flickr.js
+++ js/flickr.js
@@ -5,39 +5,41 @@
  * GistID: 5705453
  *
  */
-(function (document, $) {
-    "use strict";
+(function(document, $) {
+  'use strict';
 
-    var flickrPhotoStream = function ($el, options) {
-        var url = [
-            'http://api.flickr.com/services/feeds/photoset.gne?nsid=',
-            options.id,
-            '&set=',
-            options.setId,
-            '&format=json&jsoncallback=?'
-        ].join('');
+  var flickrPhotoStream = function($el, options) {
+    var url = [
+      'http://api.flickr.com/services/feeds/photoset.gne?nsid=',
+      options.id,
+      '&set=',
+      options.setId,
+      '&format=json&jsoncallback=?'
+    ].join('');
 
-        return $.getJSON(url).done(function (data) {
-            $.each(data.items, function (index, item) {
-                var link = item.media.m.replace('_m', '_b');
+    return $.getJSON(url).done(function(data) {
+      $.each(data.items, function(index, item) {
+        var link = item.media.m.replace('_m', '_b');
 
-                $("<img />")
-                    .attr("src", item.media.m)
-                    .appendTo($el)
-                    .wrap(options.container || '')
-                    .wrap([
-                        '<a data-lightbox="true" href="',
-                        link,
-                        options.cssClass ? '" class="' + options.cssClass : '',
-                        '" title="',
-                        item.title,
-                        '"></a>'
-                    ].join(''));
-            });
-        });
-    };
+        $('<img />')
+          .attr('src', item.media.m)
+          .appendTo($el)
+          .wrap(options.container || '')
+          .wrap(
+            [
+              '<a data-lightbox="true" href="',
+              link,
+              options.cssClass ? '" class="' + options.cssClass : '',
+              '" title="',
+              item.title,
+              '"></a>'
+            ].join('')
+          );
+      });
+    });
+  };
 
-    $.fn.flickrPhotoStream = function (options) {
-        return flickrPhotoStream($(this).get(), options || {});
-    };
+  $.fn.flickrPhotoStream = function(options) {
+    return flickrPhotoStream($(this).get(), options || {});
+  };
 })(document, jQuery);
