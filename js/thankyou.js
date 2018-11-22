b9ed8c7eaafe14c6b4f46c06ceeab4167eabe205
Ammar <binfaisal.ammar@gmail.com>
2018-11-22 20:39:21 +0530

    fixes #297 (#299)

--- js/thankyou.js
+++ js/thankyou.js
@@ -33,21 +33,21 @@ var app = document.getElementById('app');
 var count = 0;
 var index = 0;
 var typingEffect = function typingEffect() {
- var text = items[index];
- if (count < text.length) {
-   setTimeout(function () {
-     app.innerHTML += text[count];
-     count++;
-     typingEffect();
-   }, Math.floor(Math.random(10) * 100));
- } else {
-   count = 0;
-   index = index + 1 < items.length ? index + 1 : 0;
-   setTimeout(function () {
-     app.innerHTML = '';
-     typingEffect();
-   }, 3000);
- }
+  var text = items[index];
+  if (count < text.length) {
+    setTimeout(function() {
+      app.innerHTML += text[count];
+      count++;
+      typingEffect();
+    }, Math.floor(Math.random(10) * 100));
+  } else {
+    count = 0;
+    index = index + 1 < items.length ? index + 1 : 0;
+    setTimeout(function() {
+      app.innerHTML = '';
+      typingEffect();
+    }, 3000);
+  }
 };
 
 typingEffect();
