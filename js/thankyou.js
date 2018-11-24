463646c465fa8d6744f2c62ef21430c202a3e1e3
Nishith <nishithp11@gmail.com>
2018-11-24 20:32:12 +0530

    Update thankyou.js

--- js/thankyou.js
+++ js/thankyou.js
@@ -27,7 +27,7 @@
 'Go raibh maith agat as do chuid ranníocaíochtaí!',
 'Terimakasih atas kontribusi anda!',
 'Köszönjük a hozzájárulást!',
-'ചെയ്തത്തിന്',
+'സംഭാവന ചെയ്തത്തിന്',
 'Takk fyrir þitt framlag!'
 ];
 
